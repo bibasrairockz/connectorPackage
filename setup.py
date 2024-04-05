908b79a3903bf6c1b32a49d2fc95b6fc874bfc48
bibasrairockz <bibasrairockz@gmail.com>
2024-04-05 16:13:29 +0100

    push2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 REPO_NAME = "connectorPackage"
 PKG_NAME= "database-connector"
 AUTHOR_USER_NAME = "bibasrairockz"
